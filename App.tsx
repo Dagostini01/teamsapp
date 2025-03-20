7489133c0274dd6632b55905446f2d447a601b32
Dagostini01 <pedro01dagostini@gmail.com>
2025-03-20 14:56:18 -0300

    refactor: refatoracao ts

--- App.tsx
+++ App.tsx
@@ -1,7 +1,24 @@
+import { ThemeProvider } from "styled-components/native";
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto";
+
+import { Loading } from "@components/Loading";
+
+import theme from 'src/theme';
+
 import { Groups } from '@screens/Groups';
+import { StatusBar } from "react-native";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
+
   return (
-    <Groups />
+    <ThemeProvider theme={theme}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? <Groups /> : <Loading />}
+    </ThemeProvider>
   )
 }
--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,9 +1,43 @@
-import { View, Text } from "react-native";
+import { Header } from "@components/Header";
+import { Container } from "./styles";
+import { Highlight } from "@components/Hightlight";
+import { GroupCard } from "@components/GroupCard";
+import { useState } from "react";
+import { FlatList } from "react-native";
+import { ListEmpty } from "@components/ListEmpty";
+import { Button } from "@components/Button";
 
 export function Groups() {
-    return(
-        <View>
-            <Text>Groups</Text>
-        </View>
+
+    const [groups, setGroups] = useState<string[]>([]);
+
+    return (
+        <Container>
+            <Header />
+
+            <Highlight
+                title="Turmas"
+                subtitle="jogue com sua turma"
+            />
+
+            <FlatList
+                data={groups}
+                keyExtractor={item => item}
+                renderItem={({ item }) => (
+                    <GroupCard
+                        title={item}
+                    />
+                )}
+                contentContainerStyle={groups.length === 0 && { flex: 1 }}
+                ListEmptyComponent={() => (
+                    <ListEmpty
+                        message="Que tal cadastrar a primeira turma?"
+                    />
+                )}
+            />
+
+            <Button title="Criar nova turma" />
+
+        </Container>
     )
 }
